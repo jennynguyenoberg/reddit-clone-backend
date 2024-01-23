7d89e4666ea02b0bd8d6391968b46414c6497aed
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2024-01-23 15:38:51 +0100

    Fix bug

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,6 @@
 import "dotenv/config";
-import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
-import * as authController from "./controllers/auth";
-import * as postsController from "./controllers/posts";
-import * as commentsController from "./controllers/comments";
-import * as votesController from "./controllers/votes";
-import validateToken from "./middleware/validateToken";
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.post("/register", authController.register);
-app.post("/login", authController.logIn);
-app.get("/profile", validateToken, authController.profile);
-
-app.post("/posts", validateToken, postsController.create);
-app.get("/posts", postsController.getAllPosts);
-app.get("/posts/:id", postsController.getPost);
-app.put("/posts/:id/update-post", validateToken, postsController.updatePost);
-
-app.post("/posts/:postId/upvote", validateToken, votesController.upvote);
-app.post("/posts/:postId/downvote", validateToken, votesController.downvote);
-
-app.post(
-  "/posts/:postId/comments",
-  validateToken,
-  commentsController.createComment,
-);
-app.delete(
-  "/posts/:postId/comments/:commentId",
-  validateToken,
-  commentsController.deleteComment,
-);
-app.delete("/posts/:postId", validateToken, postsController.deletePost);
+import app from "./app";
 
 const mongoURL = process.env.DB_URL;
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import { Document, Schema, model } from "mongoose";
+import { Document, Schema, Model, model, MongooseError } from "mongoose";
 import bcrypt from "bcrypt";
 
 interface IUser extends Document {
@@ -7,14 +7,13 @@ interface IUser extends Document {
   createdAt: Date;
   updatedAt: Date;
 }
-
 const UserSchema = new Schema<IUser>(
   {
     userName: {
       type: String,
-      required: true,
-      unique: true,
-      trim: true,
+      required: true, 
+      unique: true, 
+      trim: true, 
     },
     password: {
       type: String,
@@ -28,12 +27,57 @@ const UserSchema = new Schema<IUser>(
 );
 
 UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) next();
+  if (!this.isModified("password")) return next();
 
-  const passwordHash = await bcrypt.hash(this.password, 10);
-  this.password = passwordHash;
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    if (error instanceof MongooseError) next(error);
+    else throw error;
+  }
 });
 
 const User = model<IUser>("User", UserSchema);
 
 export default User;
+
+// import { Document, Schema, model } from "mongoose";
+// import bcrypt from "bcrypt";
+
+// interface IUser extends Document {
+//   userName: string;
+//   password: string;
+//   createdAt: Date;
+//   updatedAt: Date;
+// }
+
+// const UserSchema = new Schema<IUser>(
+//   {
+//     userName: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//       trim: true,
+//     },
+//     password: {
+//       type: String,
+//       select: false,
+//       required: true,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   },
+// );
+
+// UserSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) next();
+
+//   const passwordHash = await bcrypt.hash(this.password, 10);
+//   this.password = passwordHash;
+// });
+
+// const User = model<IUser>("User", UserSchema);
+
+// export default User;
